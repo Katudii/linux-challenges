5e74b3dfeac50f4a69f19590da684c6032b9f13b
Thabiso Makolana <you@example.com>
2020-03-18 10:14:49 +0200

    saved task1.sh file

---
+++ task1.sh
@@ -0,0 +1,12 @@
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~$ pwd
+/home/recruit
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~$ mkdir workspace
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~$ cd workspace
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~/workspace$ ls
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~/workspace$ touch README.md
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~/workspace$ cp README.md CHANGELOG.md
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~/workspace$ ls
+CHANGELOG.md  README.md
+recruit@recruit-HP-Compaq-6000-Pro-MT-PC:~/workspace$ 
